c00a23539a7f433d3f8ca8f1777e006654f59910
Luca G. Soave <luca.soave@gmail.com>
2013-04-14 12:15:32 +0200

    TDD specs of Elasticrepo::RepoSubset

--- lib/elasticrepo/repo_subset.rb
+++ lib/elasticrepo/repo_subset.rb
@@ -1,4 +1,19 @@
 module Elasticrepo
   class RepoSubset
+    attr_reader :id, :owner, :name, :url, :description, :created_at, :pushed_at,
+                :organization, :full_name, :language, :updated_at
+    def initialize(attributes)
+      @id = attributes["id"]
+      @owner = attributes["owner"]["login"]
+      @name = attributes["name"]
+      @url = attributes["url"]
+      @description = attributes["description"]
+      @created_at = attributes["created_at"]
+      @pushed_at = attributes["pushed_at"]
+      @organization = attributes["owner"]["type"]
+      @full_name = attributes["full_name"]
+      @language = attributes["language"]
+      @updated_at = attributes["updated_at"]
+    end
   end
 end
--- spec/elasticrepo_spec.rb
+++ spec/elasticrepo_spec.rb
@@ -1,22 +1,47 @@
 require "spec_helper"
 
+  # API ref.: GET /repos/:owner/:repo
   # http://developer.github.com/v3/repos/
-  # GET /repos/:owner/:repo
 
   describe Elasticrepo::RepoSubset do
-    context "limited subset of fields" do 
-      subject { Yajl::Parser.parse(fixture("repository.json").read) }
-      its(:id) { should eq(2126244) }
-      its(:owner) { should eq("twitter") }
-      its(:name) { should eq("bootstrap") }
-      its(:url) { should eq("https://api.github.com/users/twitter") }
-      its(:description) { should eq("Sleek, intuitive, and powerful front-end framework for faster and easier web development.") }
-      its(:created_at) { should eq("2011-07-29T21:19:00Z") }
-      its(:pushed_at) { should eq("2013-04-13T03:56:36Z") }
-      its(:organization) { should eq("twitter") }
-      its(:full_name) { should eq("twitter/bootstrap") }
-      its(:language) { should eq("JavaScript") }
-      its(:updated_at) { should eq("2013-04-13T19:12:09Z") }
+    #context "read a full repository extracting the field :" do
+    context "extract a subset of repository fields" do
+      let(:parsed) { Yajl::Parser.parse(fixture("repository.json").read) }
+      subject { Elasticrepo::RepoSubset.new(parsed) }
+
+      context "#id" do
+        its(:id) { should eq(2126244) }
+      end
+      context "#owner" do
+        its(:owner) { should eq("twitter") }
+      end
+      context "#name" do
+        its(:name) { should eq("bootstrap") }
+      end
+      context "#url" do
+        its(:url) { should eq("https://api.github.com/repos/twitter/bootstrap") }
+      end
+      context "#description" do
+        its(:description) { should eq("Sleek, intuitive, and powerful front-end framework for faster and easier web development.") }
+      end
+      context "#created_at" do
+        its(:created_at) { should eq("2011-07-29T21:19:00Z") }
+      end
+      context "#pushed_at" do 
+        its(:pushed_at) { should eq("2013-04-13T03:56:36Z") }
+      end
+      context "#organization" do
+        its(:organization) { should eq("Organization") }
+      end
+      context "#full_name" do
+        its(:full_name) { should eq("twitter/bootstrap") }
+      end
+      context "#language" do
+        its(:language) { should eq("JavaScript") }
+      end
+      context "#updated_at" do
+        its(:updated_at) { should eq("2013-04-13T19:12:09Z") }
+      end
     end
   end
 

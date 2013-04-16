3c6e83c2d2b62be3b8238937fbd574b0a8ef7b0a
Luca G. Soave <luca.soave@gmail.com>
2013-04-16 17:05:05 +0200

    better spec'ing for Elasticrepo::Extractor

--- lib/elasticrepo/extractor.rb
+++ lib/elasticrepo/extractor.rb
@@ -7,12 +7,21 @@ module Elasticrepo
     # ex: results = Elasticrepo::Extractor.new(owner = "lapaty")
 
     def initialize(owner)
-      @owner = owner
-      parsed = Octokit.starred(@owner)
-      @repositories = parsed.map do |item|
-        RepoSubset.new(item)
-      end
+      @repositories = Octokit.starred(owner)
     end
+
+    # old:
+
+    ## ex: results = RepoExtractor.starred_repos_by_user(github_repo = "https://api.github.com/users/lapaty/starred")
+    #def self.starred_repos_by_user(github_repo)
+    #  # GET /users/:user/starred
+    #  # ex: /users/lapaty/starred --> array of hashes
+    #  parsed = Yajl::Parser.parse(open(github_repo).read)
+    #  repositories = parsed.map do |item|
+    #    Repository.new(item)
+    #  end
+    #end
+
   end
 end
 
--- spec/elasticrepo/extractor_spec.rb
+++ spec/elasticrepo/extractor_spec.rb
@@ -6,28 +6,32 @@ require "spec_helper"
 
   describe Elasticrepo::Extractor do
   
-      #let(:parsed) { Yajl::Parser.parse(fixture("repositories.json").read) }
-      # let(:parsed) { Octokit.starred('lapaty') }
-      # subject { Elasticrepo::Extractor.new(parsed) }
-
-      subject { Elasticrepo::Extractor.new("lapaty").repositories }
-
-      it { should be_a(Array) }
-      its(:size) { should eq(2) }
+    context "get list of repos starred by a user" do
+      describe "#new" do # Elasticrepo::Extractor.new("lapaty").repositories
+        let(:parsed )   { Yajl::Parser.parse(fixture("repositories.json").read) }
+        subject(:ockto_get) { Elasticrepo::Extractor.new("lapaty").repositories }
+  
+        # just to show the content while rspec'ing
+        it "print" do
+          print "parsed: #{parsed[1]["id"]} \n + \n +\n"
+          print "ockto_get: #{ockto_get[1]["id"]} \n + \n +\n"
+        end
+  
+        it { parsed.should be_a(Array) }
+        it { ockto_get.should be_a(Array) }
+        it { parsed.should_not be_empty }
+        it { ockto_get.should_not be_empty }
+  
+        its(:size)      { should eq(2) }
+        its(["id"])     { "#{ockto_get[1]['id']}".should eq("#{parsed[1]['id']}") }
+        its(["owner"])  { "#{ockto_get[1]['id']}".should eq("#{parsed[1]['id']}") }
+        its(["name"])   { "#{ockto_get[1]['id']}".should eq("#{parsed[1]['id']}") }
+      end
+    end
 
   end
 
-#---------------------------------------------------------------------------------
-
-#---------------------------------------------------------------------------------
-  #    it { parsed.should_not be_empty }
-
-  #    specify { parsed.should exist }
   
-  #    its([:id])  { should eq(5392501) }
-  #    its([:id]) { should eq(612595) }
-  #    its(:size) { should eq(2) }
   
-  #    its "GET /users/:user/starred" subject.should eq starred_live 
   
   

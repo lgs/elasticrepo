9de3c11776c9415e0ff09d9dc7d33b77a527d8a9
Luca G. Soave <luca.soave@gmail.com>
2013-04-26 14:35:50 +0200

    fix indexer bug

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ elasticrepo
 ===========
 
 Indexing GitHub repository with ElasticSearch
+---------------------------------------------
+
+lsoave@ubuntu:~/rails/github/elasticrepo$ gem install elasticrepo-0.0.3.gem
+Successfully installed elasticrepo-0.0.3
+1 gem installed
+lsoave@ubuntu:~/rails/github/elasticrepo$ irb 
+2.0.0-p0 :001 > require "elasticrepo"
+ => true 
+2.0.0-p0 :002 > indexer = Elasticrepo::Indexer.new("lapaty")
+ => #<Elasticrepo::Indexer:0x9bea31c @owner="lapaty", @idx="repositories"> 
+2.0.0-p0 :003 >
+
+2.0.0-p0 :004 > indexer.import
+ => #<Tire::Index:0x9df5f58 @name="repositories", @options={:mappings=>{:question=>{:properties=>{:id=>{:type=>"integer", :analyzer=>"keyword"}, :owner=>{:type=>"string", :analyzer=>"keyword"}, :name=>{:type=>"string", :analyzer=>"keyword"}, :url=>{:type=>"string", :analyzer=>"snowball"}, :description=>{:type=>"string", :analyzer=>"snowball"}, :organization=>{:type=>"string", :analyzer=>"keyword"}, :language=>{:type=>"string", :analyzer=>"keyword"}, :created_at=>{:type=>"date", :analyzer=>"keyword"}, :pushed_at=>{:type=>"date", :analyzer=>"keyword"}, :updated_at=>{:type=>"date", :analyzer=>"keyword"}}}}}, @response=#<Tire::HTTP::Response:0x9e52eb0 @body="{\"ok\":true,\"_shards\":{\"total\":10,\"successful\":5,\"failed\":0}}", @code=200, @headers={:content_type=>"application/json; charset=UTF-8", :content_length=>"60"}>> 
+2.0.0-p0 :005 >
+
+2.0.0-p0 :006 > indexer.search "ruby"
+ => #<Tire::Results::Collection:0x9e727b0 @response={"took"=>6, "timed_out"=>false, "_shards"=>{"total"=>5, "successful"=>5, "failed"=>0}, "hits"=>{"total"=>2, "max_score"=>0.6043929, "hits"=>[{"_index"=>"repositories", "_type"=>"document", "_id"=>"5392501", "_score"=>0.6043929, "_source"=>{"id"=>5392501, "owner"=>"cainlevy", "name"=>"photor", "url"=>"https://api.github.com/repos/cainlevy/photor", "description"=>"Photo Organizer (in Ruby)", "created_at"=>"2012-08-12T22:26:08Z", "pushed_at"=>"2013-02-19T03:11:10Z", "organization"=>"User", "full_name"=>"cainlevy/photor", "language"=>"Ruby", "updated_at"=>"2013-03-13T02:05:33Z"}}, {"_index"=>"repositories", "_type"=>"document", "_id"=>"612595", "_score"=>0.6043929, "_source"=>{"id"=>612595, "owner"=>"aino", "name"=>"galleria", "url"=>"https://api.github.com/repos/aino/galleria", "description"=>"The JavaScript Image Gallery", "created_at"=>"2010-04-15T21:11:51Z", "pushed_at"=>"2013-03-01T20:16:55Z", "organization"=>"Organization", "full_name"=>"aino/galleria", "language"=>"JavaScript", "updated_at"=>"2013-04-26T11:07:21Z"}}]}}, @options={}, @time=6, @total=2, @facets=nil, @max_score=0.6043929, @wrapper=Tire::Results::Item> 
+
--- lib/elasticrepo/indexer.rb
+++ lib/elasticrepo/indexer.rb
@@ -3,42 +3,51 @@ module Elasticrepo
     #attr_reader :indexed
 
     def initialize(owner)
-      @repos ||= Elasticrepo::Extractor.new(owner).repos
+      @owner = owner
+      @idx = "repositories"
     end
 
     def import
-      index = Tire::Index.new('repositories')
-      # Create the index with proper mapping (if doesn not exist already
+      # extract subset fields by user repositories starred list  
+      # GET /users/:user/starred 
       #
-      index.create :mappings => {
-        :question => {
-          :properties => {
-            :id => { :type => 'integer', :analyzer => 'keyword' },
-              :owner => { :type => 'string', :analyzer => 'keyword' },
-              :name => { :type => 'string', :analyzer => 'keyword' },
-              #:full_name => { :type => 'string', :analyzer => 'keyword' },
-              :url => { :type => 'string', :analyzer => 'snowball' },
-              :description => { :type => 'string', :analyzer => 'snowball' },
-              :organization => { :type => 'string', :analyzer => 'keyword' },
-              :language => { :type => 'string', :analyzer => 'keyword' },
-              :created_at => { :type => 'date', :analyzer => 'keyword' },
-              :pushed_at => { :type => 'date', :analyzer => 'keyword' },
-              :updated_at => { :type => 'date', :analyzer => 'keyword' }
+      repos = Elasticrepo::Extractor.new(@owner).repos
+
+      # index the extraction
+      #
+      @index = Tire::Index.new(@idx) do 
+        # Create the index with proper mapping (if doesn not exist already
+        #
+        create :mappings => {
+          :question => {
+            :properties => {
+              :id => { :type => 'integer', :analyzer => 'keyword' },
+                :owner => { :type => 'string', :analyzer => 'keyword' },
+                :name => { :type => 'string', :analyzer => 'keyword' },
+                #:full_name => { :type => 'string', :analyzer => 'keyword' },
+                :url => { :type => 'string', :analyzer => 'snowball' },
+                :description => { :type => 'string', :analyzer => 'snowball' },
+                :organization => { :type => 'string', :analyzer => 'keyword' },
+                :language => { :type => 'string', :analyzer => 'keyword' },
+                :created_at => { :type => 'date', :analyzer => 'keyword' },
+                :pushed_at => { :type => 'date', :analyzer => 'keyword' },
+                :updated_at => { :type => 'date', :analyzer => 'keyword' }
+            }
           }
-        }
-      }  
-      # Import documents
-      #
-      import @repos
-      # Refresh the index for immediate searching
-      #
-      refresh
+        }  
+        # Import documents
+        #
+        import repos
+        # Refresh the index for immediate searching
+        #
+        refresh
+      end
     end # end import
 
     def search(sub_string)
       # search for a sub_string 
       #
-      search = Tire::Search::Search.new('repositories')
+      search = Tire::Search::Search.new(@idx)
       search.query  { string('description:*"#{sub_string}"*') }
       search.results
     end
@@ -46,13 +55,13 @@ module Elasticrepo
     def update
       # Just refresh the index as is 
       #
-      refresh
+      @index.refresh
     end
 
     def delete
       # delete 'repositories' index
-      # 
-      Tire::Index.new('repositories').delete
+      #
+      @index.delete
     end
   end # end class
 end # end module

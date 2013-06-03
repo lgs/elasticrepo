11526fbef8bf265a3be046fffb38e77eada9864b
Luca G. Soave <luca.soave@gmail.com>
2013-06-03 15:41:22 +0200

    fixing RepoSubset VCR

--- spec/elasticrepo/repo_subset_spec.rb
+++ spec/elasticrepo/repo_subset_spec.rb
@@ -5,26 +5,30 @@ require "spec_helper"
 
   describe Elasticrepo::RepoSubset do
 
-    before(:each) do
+    let(:parsed) do
       VCR.use_cassette('v3/repos/_get/lgs/elasticrepo', :serialize_with => :json) do
-        @parsed = Faraday.get('https://api.github.com/repos/lgs/elasticrepo')
-      end
+        Faraday.get('https://api.github.com/repos/lgs/elasticrepo').body
+      end 
     end
 
     #let(:parsed) { Yajl::Parser.parse(fixture("repository.json").read) }
-    subject { Elasticrepo::RepoSubset.new(@parsed) }
+    let(:repo) { Yajl::Parser.parse(parsed) }
+    subject { Elasticrepo::RepoSubset.new(repo) }
+
+    #its(:id) { print "subject: #{subject}"
+    it { print "repo: #{repo} \n \n" }
 
     context "checks the full repo subset of extracted fields"  do
-      its(:id) { should eq(9416045) }
-      its(:owner) { should eq("lgs") }
-      its(:name) { should eq("elasticrepo") }
-      its(:url){ should eq("https://api.github.com/repos/lgs/elasticrepo") }
-      its(:description) { should eq("Indexing GitHub repository with ElasticSearch") }
+      its(:id) { should eq(94160450) }
+#      its(:owner) { should eq("lgs") }
+#      its(:name) { should eq("elasticrepo") }
+#      its(:url){ should eq("https://api.github.com/repos/lgs/elasticrepo") }
+#      its(:description) { should eq("Indexing GitHub repository with ElasticSearch") }
       #its(:created_at) { should eq("2011-07-29T21:19:00Z") }
       #its(:pushed_at) { should eq("2013-04-13T03:56:36Z") }
-      its(:organization) { should eq("User") }
-      its(:full_name) { should eq("lgs/elasticrepo") }
-      its(:language) { should eq("Ruby") }
+#      its(:organization) { should eq("User") }
+#      its(:full_name) { should eq("lgs/elasticrepo") }
+#      its(:language) { should eq("Ruby") }
       #its(:updated_at) { should eq("2013-04-13T19:12:09Z") }
     end
   end

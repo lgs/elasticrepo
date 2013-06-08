523563d03ac3ccc1b982038389307d0ac64c46b7
Luca G. Soave <luca.soave@gmail.com>
2013-06-09 00:08:20 +0200

    VCR with Octokit refactoring on spec/elasticrepo/extractor_spec.rb

--- spec/elasticrepo/extractor_spec.rb
+++ spec/elasticrepo/extractor_spec.rb
@@ -6,11 +6,9 @@ require "spec_helper"
 
     describe Elasticrepo::Extractor, :vcr => { :cassette_name => "v3/users/_get/lapaty/starred" } do
 
-      let(:fixture_repos)  { Yajl::Parser.parse(fixture("repositories.json").read) }
-      let(:live_extracted) { Elasticrepo::Extractor.new("lapaty") }
+      subject(:extracted) { Elasticrepo::Extractor.new("lapaty") }
 
-      describe "#extract" do 
-
+      describe "#new" do
     #    describe "GET /repos/:owner/:repo" do
     #      it 'gets "lgs/elasticrepo" repository (JSON)' do
     #        VCR.use_cassette('v3/repos/#get/lgs/elasticrepo') do
@@ -23,49 +21,28 @@ require "spec_helper"
     #        end
     #      end
     #    end
-        
-        #context "specs debugging" do
-        #  it "print the content of running specs" do
-        #    print "live_extracted.repos:                 #{live_extracted.repos} \n \n" 
-        #    print "live_extracted.repos.class:           #{live_extracted.repos.class} \n \n"
-        #    print "live_extracted.repos.to_json:         #{live_extracted.repos.to_json} \n \n"
-        #    print "live_extracted.repos[0].to_json:      #{live_extracted.repos[0].to_json} \n \n"
-        #    print "live_extracted.repos[0].owner:        #{live_extracted.repos[0].owner} \n \n"
-        #    print "live_extracted.repos[0].organization: #{live_extracted.repos[0].organization} \n \n"
 
-        #    print "fixture_repos:                        #{fixture_repos} \n \n"
-        #    print "fixture_repos.class:                  #{fixture_repos.class} \n \n"
-        #    print "fixture_repos.to_json:                #{fixture_repos[0].to_json} \n \n"
-        #    print "fixture_repos[0].to_json:             #{fixture_repos[0].to_json} \n \n"
-        #    print "fixture_repos[0]['owner']['login']:   #{fixture_repos[0]['owner']['login']} \n \n"
-        #    print "fixture_repos[0]['organization']:     #{fixture_repos[0]['organization']} \n \n"
-        #  end
-        #end
+        context "debugging specs print out" do
+          it "print the content of running specs" do
+            print "extracted.repos.class:                  #{extracted.repos.class} \n \n"        
+            print "extracted.repos.inspect:                #{extracted.repos.inspect} \n \n"
+            print "extracted.repos[0].id:                  #{extracted.repos[0].id} \n \n"
+            print "extracted.repos[0].owner                #{extracted.repos[0].owner} \n \n"
+          end
+        end
 
-        context "extract fields subset from each repo" do          
-          its("fixture_repos[0]['id']") { fixture_repos[0]['id'].should eq live_extracted.repos[0].id }
-          its("fixture_repos[0]['owner']['login']") { fixture_repos[0]['owner']['login'].should eq live_extracted.repos[0].owner['login'] }
-          its("fixture_repos[0]['name']") { fixture_repos[0]['name'].should eq live_extracted.repos[0].name }
-          its("fixture_repos[0]['url']") { fixture_repos[0]['url'].should eq live_extracted.repos[0].url }
-          its("fixture_repos[0]['description']") { fixture_repos[0]['description'].should eq live_extracted.repos[0].description }
-          its("fixture_repos[0]['created_at']") { fixture_repos[0]['created_at'].should eq live_extracted.repos[0].created_at }
-          its("fixture_repos[0]['pushed_at']") { fixture_repos[0]['pushed_at'].should eq live_extracted.repos[0].pushed_at }
-          its("fixture_repos[0]['organization']") { fixture_repos[0]['organization'].should eq live_extracted.repos[0].organization }
-          its("fixture_repos[0]['language']") { fixture_repos[0]['language'].should eq live_extracted.repos[0].language }
-          its("fixture_repos[0]['full_name']") { fixture_repos[0]['full_name'].should eq live_extracted.repos[0].full_name }
-          its("fixture_repos[0]['updated_at']") { fixture_repos[0]['updated_at'].should eq live_extracted.repos[0].updated_at }
-
-          its("fixture_repos[1]['id']") { fixture_repos[1]['id'].should eq live_extracted.repos[1].id }
-          its("fixture_repos[1]['owner']['login']") { fixture_repos[1]['owner']['login'].should eq live_extracted.repos[1].owner['login'] }
-          its("fixture_repos[1]['url']") { fixture_repos[1]['url'].should eq live_extracted.repos[1].url }
-          its("fixture_repos[1]['description']") { fixture_repos[1]['description'].should eq live_extracted.repos[1].description }
-          its("fixture_repos[1]['name']") { fixture_repos[1]['name'].should eq live_extracted.repos[1].name }
-          its("fixture_repos[1]['created_at']") { fixture_repos[1]['created_at'].should eq live_extracted.repos[1].created_at }
-          its("fixture_repos[1]['pushed_at']") { fixture_repos[1]['pushed_at'].should eq live_extracted.repos[1].pushed_at }
-          its("fixture_repos[1]['organization']") { fixture_repos[1]['organization'].should eq live_extracted.repos[1].organization }
-          its("fixture_repos[1]['full_name']") { fixture_repos[1]['full_name'].should eq live_extracted.repos[1].full_name }
-          its("fixture_repos[1]['language']") { fixture_repos[1]['language'].should eq live_extracted.repos[1].language }
-          its("fixture_repos[1]['updated_at']") { fixture_repos[1]['updated_at'].should eq live_extracted.repos[1].updated_at }
+        context "extract fields subset from each repo" do   
+         its("repos[0]['id']")           { extracted.repos[0].id.should eq 5392501 }
+         its("repos[0]['owner']")        { extracted.repos[0].owner.should eq "cainlevy" }
+         its("repos[0]['url']")          { extracted.repos[0].url.should eq "https://api.github.com/repos/cainlevy/photor" }
+         its("repos[0]['description']")  { extracted.repos[0].description.should eq "Photo Organizer (in Ruby)" }
+         its("repos[0]['name']")         { extracted.repos[0].name.should eq "photor" }
+         its("repos[0]['created_at']")   { extracted.repos[0].created_at.should eq "2012-08-12T22:26:08Z" }
+         its("repos[0]['pushed_at']")    { extracted.repos[0].pushed_at.should eq "2013-02-19T03:11:10Z" }
+         its("repos[0]['organization']") { extracted.repos[0].organization.should eq "User" }
+         its("repos[0]['full_name']")    { extracted.repos[0].full_name.should eq "cainlevy/photor" }
+         its("repos[0]['language']")     { extracted.repos[0].language.should eq "Ruby" }
+         its("repos[0]['updated_at']")   { extracted.repos[0].updated_at.should eq "2013-03-13T02:05:33Z" }
         end
 
       end 
